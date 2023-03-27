0d85865b7d28cca6fad8f4e5bdafbe1b525d83e0
DevAntonioRogers <ar919061493@gmail.com>
2023-03-26 21:26:08 -0400

    Migrated everything to Redux Toolkit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,8 @@
 import classes from './Counter.module.css';
 import { useSelector, useDispatch } from 'react-redux';
 
+import { counterActions } from '../store';
+
 const Counter = () => {
 
   const dispatch = useDispatch()
@@ -8,19 +10,19 @@ const Counter = () => {
   const toggle = useSelector(state => state.showCounter)
 
   const incrementHandler = () => {
-    dispatch({ type: 'increment' })
+    dispatch(counterActions.increment())
   }
 
   const increaseHandler = () => {
-    dispatch({ type: 'increase', amount: 5 })
+    dispatch(counterActions.increase(5))
   }
 
   const decrementHandler = () => {
-    dispatch({ type: 'decrement' })
+    dispatch(counterActions.decrement())
   }
 
   const toggleCounterHandler = () => {
-    dispatch({ type: 'toggle' })
+    dispatch(counterActions.togglerCounter())
   };
 
   return (
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,8 @@
-import { createStore } from 'redux'
-
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, configureStore } from '@reduxjs/toolkit'
 
 const initialState = { counter: 0, showCounter: true }
 
-createSlice({
+const counterSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
@@ -15,7 +13,7 @@ createSlice({
       state.counter--
     },
     increase(state, action) {
-      state.counter = state.counter + action.amount
+      state.counter = state.counter + action.payload
     },
     togglerCounter(state) {
       state.showCounter = !state.showCounter
@@ -23,39 +21,11 @@ createSlice({
   }
 })
 
-const counterReducer = (state = initialState, action) => {
-  if (action.type === 'increment') {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter
-    }
-  }
 
-  if (action.type === 'increase') {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter
-    }
-  }
+const store = configureStore({
+  reducer: counterSlice.reducer
+})
 
-  if (action.type === 'decrement') {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter
-    }
-  }
-
-  if (action.type === 'toggle') {
-    return {
-      showCounter: !state.showCounter,
-      counter: state.counter
-    }
-  }
-  return state
-}
-
-
-
-const store = createStore(counterReducer)
+export const counterActions = counterSlice.actions
 
 export default store
