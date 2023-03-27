9469b5ec6bb264039b761fdee1638ddba27b9300
DevAntonioRogers <ar919061493@gmail.com>
2023-03-26 20:31:37 -0400

    Created Store

---
+++ src/components/Counter.js
@@ -0,0 +1,19 @@
+import classes from './Counter.module.css';
+import { useSelector } from 'react-redux';
+
+const Counter = () => {
+
+  const counter = useSelector(state => state.counter)
+
+  const toggleCounterHandler = () => { };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
